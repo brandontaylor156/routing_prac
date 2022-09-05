f81ac9547865a4675ae40b497db442333023e40e
brandontaylor156 <brandon.david4@outlook.com>
2022-09-04 19:41:14 -0700

    skywalker attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,68 @@
 import React from 'react';
-import {Routes, Route, Link} from 'react-router-dom'
-import {useParams} from "react-router"
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 import style from './App.module.css'
 
 
-// import React, {useState} from 'react'
-// import { useNavigate } from 'react-router-dom';
+const App = () => {
+  const [category, setCategory] = React.useState("people");
+  const [id, setId] = React.useState(1);
+  const [name, setName] = React.useState("");
+  const [submitted, setSubmitted] = React.useState(false);
 
-// const App = () => {
-//   const [name, setName] = useState("");
-//   const [comment, setComment] = useState("");
-//   const navigate = useNavigate();
-
-//   const sendSurvey = (e) => {
-//     e.preventDefault();
-
-//     navigate("/results");
-//   }
-
-//   return (
-//     <form onSubmit={sendSurvey}>
-//       <label>Your name:</label>
-//       <input type="text" onChange={(e)=> setName(e.target.value)} value={name} />
-
-//       <label>Your comment:</label>
-//       <textarea onChange={(e) => setComment(e.target.value)} value={comment} />
-
-//       <input type="submit" value="Submit survey" />
-//     </form>
-
-//   )
-// }
-
-// const Location = () => {
-//   const {city} = useParams();
-//   return (
-//     <div>
-//       <h1>Welcome to {city}!</h1>
-//     </div>
-//   );
-// }
-
-const Home = () => {
-  return (
-    <div className={style.fadeIn}>
-      <h1>Welcome home</h1>
-    </div>
-  );
-}
-
-const Argument = () => {
-  const {argument} = useParams();
+  const navigate = useNavigate();
+  
+  React.useEffect(()=> {
+    axios.get(`https://swapi.dev/api/${category}/${id}`)
+      .then(response=>{setName(response.data.name)})
+  }, [category, id])
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+    navigate(`/${category}/${id}`);
+  }
 
   return (
-    <div className={style.fadeIn}>
+    <div className={style.flex}>
+      <div>
+        <form onSubmit={handleSubmit}>
+
+          <label>Search for:</label>
+          <select 
+            name="category" 
+            onChange={(e)=> {
+              setCategory(e.target.value);
+              setSubmitted(false);
+            }}
+            value={category}
+            >
+            <option value="people">People</option>
+            <option value="planets">Planets</option>
+          </select>
+          
+          &nbsp;&nbsp;&nbsp;&nbsp;
+
+          <label>ID:</label>
+          <input 
+            type="number" 
+            min="1" 
+            onChange={(e) => { 
+              setId(e.target.value);
+              setSubmitted(false);
+            }} 
+            value={id}
+            />     
+          <button>Search</button>
+        </form>
+      </div>
+
       {
-      isNaN(argument) ? 
-      <h1>The word is: {argument}</h1> :
-      <h1>The number is: {argument}</h1>
+      submitted && 
+      <div>
+        <p>Name: {name}</p>
+      </div>
       }
-    </div>
-  )
-}
-
-const Multiple = () => {
-  const {word, color1, color2} = useParams();
-  
-  return (
-    <div className={style.fadeIn}>
-      <h1 style={{color:color1, backgroundColor:color2}}>{word}</h1>
-    </div>
-  )
-}
-
-
-
-function App() {
-  return (
-  
-    <div>
-      {/* <p>
-        <Link to="/location/seattle">Seattle</Link>
-         | 
-        <Link to="/location/chicago">Chicago</Link>
-         | 
-        <Link to="/location/burbank">Burbank</Link>
-      </p> */}
-      <Routes>
-        <Route path="/home" element={<Home/>} />
-        <Route path="/:argument" element={<Argument/>}/>
-        <Route path="/:word/:color1/:color2" element={<Multiple/>} />
-        {/* <Route path="/location/:city" element={<Location />} /> */}
-      </Routes>
       
     </div>
 
